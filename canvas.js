3087eef4e1f7047ef03e03210a4911f5be015b5d
Fede <fedeboccazzi@gmail.com>
2022-02-08 15:59:33 -0300

    funcion crearTablero, dibujarLineaLetra

--- canvas.js
+++ canvas.js
@@ -2,3 +2,23 @@ var pantalla = document.querySelector('#ahorcado');
 // Oculto canvas hasta que inicia juego
 pantalla.style.display = 'none';
 var pincel = pantalla.getContext("2d");
+
+function crearTablero() {
+    pincel.fillStyle = "lightgrey"
+    pincel.fillRect(0,0,800,1200);
+    
+    // Muestra canvas al click del boton
+    if (pantalla.style.display == 'none') {
+        pantalla.style.display = 'block';
+    } else {
+        pantalla.style.display = 'none';
+    }
+}
+
+function dibujarLineaLetra() {
+    pincel.fillStyle = "black";
+    pincel.beginPath();
+    pincel.moveTo(700, 600);
+    pincel.lineTo(750, 600);
+    pincel.stroke();
+}
