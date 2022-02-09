c3bdc5c9e3464f783e57273d96c406b67991638e
Fede Ciclo <tecciclotron@fleni.org.ar>
2022-02-09 08:53:20 -0300

    agregado funciones para dibujar ahorcado completo

--- canvas.js
+++ canvas.js
@@ -13,16 +13,99 @@ function crearTablero() {
     } else {
         pantalla.style.display = 'none';
     }
+    pincel.fillStyle = "black"; // color del trazo para dibujo y letras
 }
 
 function dibujarLineaLetra(palabra) {
-    var pos = 300;
+    var pos = 320;
     for (var i=0; i < palabra.length; i++){
-        pincel.fillStyle = "black";
         pincel.beginPath();
         pincel.moveTo(pos, 600);
         pincel.lineTo((pos + 50), 600);
         pincel.stroke();
         pos = pos + 70; // dejo un espacio en blanco para el siguiente guion
+    }
+}
+
+function dibujarBase() {
+    pincel.strokeRect (10,500, 280, 100);
+}
+
+function dibujarMastilVert1() {
+    pincel.beginPath();
+    pincel.moveTo(145, 500);
+    pincel.lineTo(145, 100);
+    pincel.stroke();
+}
+
+function dibujarMastilHorz() {
+    pincel.beginPath();
+    pincel.moveTo(145, 100);
+    pincel.lineTo(300, 100);
+    pincel.stroke();
+}
+
+function dibujarMastilVert2() {
+    pincel.beginPath();
+    pincel.moveTo(300, 100);
+    pincel.lineTo(300, 150);
+    pincel.stroke();
+}
+
+function dibujarCabeza() {
+    pincel.beginPath()
+    pincel.arc(300, 175, 25, 0, Math.PI * 2);
+    pincel.stroke()
+}
+
+function dibujarCuerpo() {
+    pincel.beginPath();
+    pincel.moveTo(300, 200);
+    pincel.lineTo(300, 215);
+    pincel.stroke();
+    pincel.strokeRect(270,215,60,100)
+}
+
+function dibujarBrazo(cual) {
+    var posX = 0;
+    if (cual == 'der') {
+        posX = 330;
+    }  else {
+        posX = 270;
+    }
+    pincel.beginPath();
+    pincel.moveTo(posX, 225);
+    if (cual == 'der') {
+        pincel.lineTo(posX + 20, 290);
+        pincel.moveTo(posX + 20, 290);
+        pincel.arc(posX + 25, 295, 5, 0, Math.PI *2);
+        pincel.stroke();
+    } else {
+        pincel.lineTo(posX - 20, 290);
+        pincel.moveTo(posX - 20, 290);
+        pincel.arc(posX - 25, 295, 5, 0, Math.PI *2);
+        pincel.stroke();
+    }
+}
+
+function dibujarPierna (cual) {
+    var posX = 0;
+    if (cual == 'der') {
+        posX = 320;
+    }  else {
+        posX = 280;
+    }
+    pincel.beginPath();
+    pincel.moveTo(posX, 315);
+    if (cual == 'der') {
+        pincel.lineTo(posX, 415);
+        pincel.moveTo(posX, 415);
+        pincel.arc(posX, 420, 5, 0, Math.PI *2);
+        pincel.stroke();
+    } else {
+        pincel.lineTo(posX, 415);
+        pincel.moveTo(posX, 415);
+        pincel.arc(posX, 420, 5, 0, Math.PI *2);
+        pincel.stroke();
     }
 }
