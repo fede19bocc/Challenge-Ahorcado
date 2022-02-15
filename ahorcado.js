59f9f35a4e139398fa52963e56c9b512cc52334a
Fede Ciclo <tecciclotron@fleni.org.ar>
2022-02-15 10:04:49 -0300

    dibuja letra o ahorcado segun la tecla presionada

--- ahorcado.js
+++ ahorcado.js
@@ -2,13 +2,42 @@ var botonIniciar = document.querySelector('#iniciar-juego');
 var botonAgregarPalabra = document.querySelector('#nueva-palabra');
 var inputPalabraSecreta = document.querySelector('#input-nueva-palabra');
 var palabrasSecretras = ["hola", "mundo", "cochera", "palacio", "murcielago"];
+var juegoIniciado = false;
+var palabraSecreta = "";
+var ahorcado = 0;
 
-botonIniciar.addEventListener('click', function(){
+var letrasCorrectas = [];
+var letrasIncorrectas = [];
+
+botonIniciar.addEventListener('click', function(evento){
     crearTablero();
-    let palabraSecreta = escogerPalabra();
-    console.log(palabraSecreta);
+    palabraSecreta = escogerPalabra();
     dibujarLineaLetra(palabraSecreta); 
+    dibujarBase();
+    evento.preventDefault();
+    juegoIniciado = true;
+    console.log(palabraSecreta);
+    document.addEventListener('keypress', teclado);
 });
+
+function teclado(tecla) {
+    dibujarLetra(buscaLetra(tecla.key, palabraSecreta), palabraSecreta);
+    if (buscaLetra(tecla.key, palabraSecreta).length == 0) {
+        dibujarAhorcado(ahorcado);
+        ahorcado++;
+    }
+}
+
+function dibujarAhorcado(numero) {
+    if (numero == 0) {return dibujarMastilVert1(), dibujarMastilHorz(), dibujarMastilVert2();}
+    else if (numero == 1) {return dibujarCabeza();}
+    else if (numero == 2) {return dibujarCuerpo();}
+    else if (numero == 3) {return dibujarBrazo('der');}
+    else if (numero == 4) {return dibujarBrazo('izq');}
+    else if (numero == 5) {return dibujarPierna('der');}
+    else if (numero == 6) {return dibujarPierna('izq');}
+    else { alert("perdiste!!!");}
+}
 
 botonAgregarPalabra.addEventListener('click', function(evento) {
     palabrasSecretras.push(inputPalabraSecreta.value);
@@ -22,8 +51,7 @@ function escogerPalabra() {
 }
 
 //Devuelve un array con las posiciones de la letra buscada, si no esta es vacio
-function buscaLetra(letra, palabraSecreta) {
-    let palabra = palabraSecreta;
+function buscaLetra(letra, palabra) {
     let numCar = palabra.length; //longitud del texto
     var lugar = []; //guardo la posicion de la letra
     let posicion = palabra.indexOf(letra); //primera aparición de letra
